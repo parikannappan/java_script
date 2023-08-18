89ab17e694533bf5b1ffbd0814b4eb0c4b0a9646
pari kannappan <44460396+parikannappan@users.noreply.github.com>
2023-08-18 18:32:54 +0530

    Add files via upload

---
+++ Examples.js
@@ -0,0 +1,11 @@
+let a1 = 10;
+let b1 = 23;
+let firstName = 'pari';
+const perct = 0.4;
+let isAppoved = true;
+let person = {name: 'kannappan', age: 60};
+let person1 = {};
+person1.name1 = 'kris';
+person1.age1 = 90;
+console.log(a1);
+console.log(firstName,a1+b1,perct,isAppoved,person,person1); 
